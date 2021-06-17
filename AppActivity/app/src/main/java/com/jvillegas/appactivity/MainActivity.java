4f803a69c6cd07fefdd6eabcc8e1adc4a362e5f9
junjvil <junjvil@gmail.com>
2021-06-17 16:59:14 -0600

    Actualizacion
    Actualizacion de la AppActivity

--- AppActivity/app/src/main/java/com/jvillegas/appactivity/MainActivity.java
+++ AppActivity/app/src/main/java/com/jvillegas/appactivity/MainActivity.java
@@ -2,7 +2,9 @@ package com.jvillegas.appactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,25 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"OnCreate",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"Operacion OnCreate MainActivity",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Toast.makeText(this,"OnStart",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"Operacion OnStart MainActivity",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Toast.makeText(this,"OnReset",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"Operacion OnReset MainActivity",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Toast.makeText(this,"OnStop",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"Operacion OnStop MainActivity",Toast.LENGTH_SHORT).show();
     }
 
     // se activa cuando se cirera la aplicacion
@@ -37,18 +39,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Toast.makeText(this,"OnDestroy",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"Operacion OnDestroy MainActivity",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Toast.makeText(this,"OnPause",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"Operacion OnPause MainActivity",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Toast.makeText(this,"OnResumen",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"Opearcion OnResumen MainActivity",Toast.LENGTH_SHORT).show();
+    }
+
+    public void Cambio(View view) {
+        Intent inte=new Intent(this,OtherActivity.class);
+        startActivity(inte);
     }
 }
--- AppActivity/app/src/main/java/com/jvillegas/appactivity/OtherActivity.java
+++ AppActivity/app/src/main/java/com/jvillegas/appactivity/OtherActivity.java
@@ -3,6 +3,7 @@ package com.jvillegas.appactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class OtherActivity extends AppCompatActivity {
 
@@ -10,5 +11,43 @@ public class OtherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+        Toast.makeText(this,"Operacion OnCreate OtherActivity",Toast.LENGTH_SHORT).show();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"Oparacion onStart OtherActivity",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this,"Operacion OnRestart OtherActivity",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,"Operacion onStop OtherActivity",Toast.LENGTH_SHORT ).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this,"Operacion onDestroy OtherActivity",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this,"Operacion onPause OtherActivity",Toast.LENGTH_SHORT ).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this,"Operacion onResumen OtherActivity",Toast.LENGTH_SHORT).show();
+    }
+
 }
